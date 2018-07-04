0e6c6a5665f03358180ea582eece480a159ba3c0
lukw510903926 <13507615840@163.com>
2018-07-04 21:57:06 +0800

    动态路由数据库配置

---
+++ cloud-zuul/src/main/java/com/tykj/cloud/zuul/route/RefreshController.java
@@ -0,0 +1,29 @@
+package com.tykj.cloud.zuul.route;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.zuul.web.ZuulHandlerMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RefreshController {
+
+    @Autowired
+    private RefreshRouteServiceImpl refreshRouteService;
+
+    @Autowired
+    ZuulHandlerMapping zuulHandlerMapping;
+
+    @GetMapping("/refreshRoute")
+    public String refresh() {
+
+        refreshRouteService.refreshRoute();
+        return "refresh success";
+    }
+
+    @GetMapping("/watchRoute")
+    public Object watchNowRoute() {
+
+        return zuulHandlerMapping.getHandlerMap();
+    }
+}
