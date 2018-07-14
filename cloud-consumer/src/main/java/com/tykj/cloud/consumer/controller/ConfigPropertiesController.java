c4fbd60eadf8217ec3c1a3101ff0ec8d0e3a3048
lukw510903926 <13507615840@163.com>
2018-07-14 10:15:46 +0800

    添加自定义header

--- cloud-consumer/src/main/java/com/tykj/cloud/consumer/controller/ConfigPropertiesController.java
+++ cloud-consumer/src/main/java/com/tykj/cloud/consumer/controller/ConfigPropertiesController.java
@@ -1,9 +1,7 @@
 package com.tykj.cloud.consumer.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/config")
 public class ConfigPropertiesController {
-
-    @Autowired
-    private Environment environment;
 
     @Value("${spring.cloud.config.msg}")
     private String msg;
