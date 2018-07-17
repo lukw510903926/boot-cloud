06cca146ee0581333e1595fccdd882f85d9fbddc
lkw510903926 <13507615840@.163>
2018-07-17 16:24:59 +0800

    客户端接入

--- cloud-producter/src/main/java/com/tykj/cloud/producter/ProducterApplication.java
+++ cloud-producter/src/main/java/com/tykj/cloud/producter/ProducterApplication.java
@@ -6,14 +6,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.http.MediaType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableEurekaClient
 @SpringCloudApplication
+@EnableFeignClients(basePackages = "com.tykj.cloud.security.feign")
 public class ProducterApplication {
 
 	private static Logger logger = LoggerFactory.getLogger(ProducterApplication.class);
@@ -32,6 +38,25 @@ public class ProducterApplication {
 		fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
 		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
 		fastConverter.setFastJsonConfig(fastJsonConfig);
+		List<MediaType> supportedMediaTypes = new ArrayList<>();
+		supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+		supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+		supportedMediaTypes.add(MediaType.APPLICATION_ATOM_XML);
+		supportedMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+		supportedMediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
+		supportedMediaTypes.add(MediaType.APPLICATION_PDF);
+		supportedMediaTypes.add(MediaType.APPLICATION_RSS_XML);
+		supportedMediaTypes.add(MediaType.APPLICATION_XHTML_XML);
+		supportedMediaTypes.add(MediaType.APPLICATION_XML);
+		supportedMediaTypes.add(MediaType.IMAGE_GIF);
+		supportedMediaTypes.add(MediaType.IMAGE_JPEG);
+		supportedMediaTypes.add(MediaType.IMAGE_PNG);
+		supportedMediaTypes.add(MediaType.TEXT_EVENT_STREAM);
+		supportedMediaTypes.add(MediaType.TEXT_HTML);
+		supportedMediaTypes.add(MediaType.TEXT_MARKDOWN);
+		supportedMediaTypes.add(MediaType.TEXT_PLAIN);
+		supportedMediaTypes.add(MediaType.TEXT_XML);
+		fastConverter.setSupportedMediaTypes(supportedMediaTypes);
 		return new HttpMessageConverters(fastConverter);
 	}
 }
--- cloud-producter/src/main/java/com/tykj/cloud/producter/config/ApplicationConfig.java
+++ cloud-producter/src/main/java/com/tykj/cloud/producter/config/ApplicationConfig.java
@@ -1,6 +1,9 @@
 package com.tykj.cloud.producter.config;
 
 import com.tykj.cloud.api.filter.HeaderFilter;
+import com.tykj.cloud.security.feign.LoginFeign;
+import com.tykj.cloud.security.filter.AuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +14,10 @@ import org.springframework.context.annotation.Configuration;
  * @create 2018-07-11 19:17
  **/
 @Configuration
-public class ApplicatonConfig {
+public class ApplicationConfig {
+
+    @Autowired
+    private LoginFeign loginFeign;
 
     @Bean
     public FilterRegistrationBean headerFilter() {
@@ -22,6 +28,19 @@ public class ApplicatonConfig {
         registration.addInitParameter("header-filter", "header-filter-value");
         registration.setName("header-filter");
         registration.setOrder(1);
+        return registration;
+    }
+
+    @Bean
+    public FilterRegistrationBean authenticationFilter() {
+
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setLoginFeign(loginFeign);
+        registration.setFilter(authenticationFilter);
+        registration.addUrlPatterns("/*");
+        registration.setName("authenticationFilter");
+        registration.setOrder(2);
         return registration;
     }
 }
