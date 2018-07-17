80bc2db427106969bd8c116ebb59614f559db43c
lkw510903926 <13507615840@.163>
2018-07-17 16:25:34 +0800

    客户端接入

---
+++ cloud-security/cloud-security-api/src/main/java/com/tykj/cloud/security/util/Constants.java
@@ -0,0 +1,26 @@
+package com.tykj.cloud.security.util;
+
+/**
+ * 常量定义
+ *
+ * @author lukw
+ * @email 13507615840@163.com
+ * @create 2018-07-17 16:22
+ **/
+public class Constants {
+
+    /**
+     * 认证客户端标识
+     */
+    public final static String CLOUD_CLIENT_ID = "cloud_client_id";
+
+    /**
+     * 认证客户端key
+     */
+    public final static String CLOUD_CLIENT_KEY = "cloud_client_key";
+
+    /**
+     * 认证客户端 token
+     */
+    public final static String CLOUD_CLIENT_TOKEN = "cloud_client_token";
+}
