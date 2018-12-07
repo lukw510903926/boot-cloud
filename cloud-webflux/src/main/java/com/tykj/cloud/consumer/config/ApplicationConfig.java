0dc900ca37b4aab4d4ef61fe88d7758b15e56947
yangqi <13507615840@163.com>
2018-12-07 16:23:34 +0800

    contextPath 修改

--- cloud-webflux/src/main/java/com/tykj/cloud/consumer/config/ApplicationConfig.java
+++ cloud-webflux/src/main/java/com/tykj/cloud/consumer/config/ApplicationConfig.java
@@ -3,11 +3,11 @@ package com.tykj.cloud.consumer.config;
 import com.tykj.cloud.security.autoconfigure.SsoClientProperties;
 import com.tykj.cloud.security.util.Constants;
 import feign.RequestInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 
 /**
@@ -18,31 +18,26 @@ import org.springframework.web.server.WebFilter;
 @Configuration
 public class ApplicationConfig {
 
-    @Autowired
-    private ServerProperties serverProperties;
-
-    @Autowired
-    private SsoClientProperties ssoClientProperties;
-
     @Bean
-    public WebFilter contextPathWebFilter() {
+    public WebFilter contextPathWebFilter(ServerProperties serverProperties) {
+
         String contextPath = serverProperties.getServlet().getContextPath();
         return (exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             if (request.getURI().getPath().startsWith(contextPath)) {
-                return chain.filter(
-                        exchange.mutate()
-                                .request(request.mutate().contextPath(contextPath).build())
-                                .build());
+                ServerWebExchange build = exchange.mutate()
+                        .request(request.mutate().contextPath(contextPath).build())
+                        .build();
+                return chain.filter(build);
             }
             return chain.filter(exchange);
         };
     }
 
     @Bean
-    public RequestInterceptor requestInterceptor() {
+    public RequestInterceptor requestInterceptor(SsoClientProperties ssoClientProperties) {
 
-        return requestTemplate -> requestTemplate.header("CLOUD_HEADER",  "CLOUD_FEIGN_HEADER_VALUE")
+        return requestTemplate -> requestTemplate.header("CLOUD_HEADER", "CLOUD_FEIGN_HEADER_VALUE")
                 .header(Constants.CLOUD_CLIENT_ID, ssoClientProperties.getClientId())
                 .header(Constants.CLOUD_CLIENT_KEY, ssoClientProperties.getClientKey())
                 .header(Constants.CLOUD_CLIENT_TOKEN, Constants.CLOUD_CLIENT_TOKEN);
