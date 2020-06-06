31ee17811cbcf16411c0b2c60553463cfe4c61d4
yangqi <lkw510903926@163.com>
2020-06-06 22:27:25 +0800

    alibaba 组件替换

---
+++ cloud-alibaba/cloud-alibaba-producer/src/main/java/com/boot/cloud/alibaba/producer/AlibabaProducerApplication.java
@@ -0,0 +1,22 @@
+package com.boot.cloud.alibaba.producer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author : yangqi
+ * @email : lukewei@mockuai.com
+ * @description :
+ * @since : 2020-06-06 22:11
+ */
+@Slf4j
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AlibabaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaConsumerApplication.class, args);
+    }
+}
