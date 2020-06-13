404bb73bb31f9003d5b121cae7510a4d04d5285a
yangqi <lkw510903926@163.com>
2020-06-13 20:38:47 +0800

    nacos okhttp

--- cloud-webflux/src/main/java/com/cloud/consumer/WebFluxApplication.java
+++ cloud-webflux/src/main/java/com/cloud/consumer/WebFluxApplication.java
@@ -1,5 +1,6 @@
 package com.cloud.consumer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -7,16 +8,15 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@Slf4j
 @EnableEurekaClient
 @SpringCloudApplication
 @EnableFeignClients(basePackages = "com.cloud.api")
 public class WebFluxApplication {
 
-    private static Logger logger = LoggerFactory.getLogger(WebFluxApplication.class);
-
     public static void main(String[] args) {
 
         SpringApplication.run(WebFluxApplication.class, args);
-        logger.info("WebFluxApplication Application start successfully------------");
+        log.info("WebFluxApplication Application start successfully------------");
     }
 }
