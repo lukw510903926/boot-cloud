b24ddb1a4d42a9c1fd640333fce088c0cf049f77
yangqi <lkw510903926@163.com>
2020-06-13 20:39:15 +0800

    nacos okhttp

--- cloud-webflux/src/main/java/com/cloud/consumer/WebFluxApplication.java
+++ cloud-webflux/src/main/java/com/cloud/consumer/WebFluxApplication.java
@@ -1,8 +1,6 @@
 package com.cloud.consumer;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
